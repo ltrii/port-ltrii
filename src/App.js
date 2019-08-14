00b6b62a250f819c8dd555344b05aea801191664
ltrii <lelandtr@gmail.com>
2019-08-13 17:43:00 -0700

    Init with setup ready

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+
+import TopBar from './components/TopBar';
+import JumboMain from './components/JumboMain';
+import About from './components/About';
+import Projects from './components/Projects';
+import Content from './components/Content';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="funcHold">
+        <TopBar />
+        <JumboMain />
+        <About />
+        <Projects />
+        <Content />
+        <Footer />
+      </div>
+    </div>
+  );
+}
+
+export default App;
