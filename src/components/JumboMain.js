04e22c150dd42f8556635ef49e6e9fde3193dc86
ltrii <lelandtr@gmail.com>
2019-08-29 12:57:02 -0700

    Latest changes

--- src/components/JumboMain.js
+++ src/components/JumboMain.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import './css/jumbo.css';
 
+import lambdaImg from '../assets/img/lambda.png'
+
 export default function JumboMain() {
     return (
         <div className="jumboMain">
             <div className="bgHolder" />
             <div className="topJumbo">
-                <div className="text-shadow-pop-tl">
-                    Hi! I'm Leland.<br />
+                <div className="jumboText">
+                    Hi! I'm Leland Rogers, a full stack software engineer living in the Bay Area of California who is attending Lambda School. I'd love to work for you.
                 </div>
             </div>
             <div className="bottomJumbo">
-                <div>a pretty good Full Stack web developer</div>
+                <div>Where I'm attending: </div>
+                <img src={lambdaImg} alt="Lambda School" />
             </div>
         </div>
     )
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -12,7 +12,9 @@ export default function Projects() {
     return (
         <div className="projects" name="projects">
             <div className="innerProj">
-                Projects
+                <h3>
+                    Projects
+                </h3>
                 <div className="mapHolder">
                     {projArray.map(function(item){
                     return <ProjCard project={item} key={Math.random()}/>
