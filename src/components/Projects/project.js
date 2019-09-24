e0a78b1bdfbeb4b47a2046dcd06179266eecc142
ltrii <lelandtr@gmail.com>
2019-09-23 23:25:24 -0700

    Some work on the wording in projects. NPM updates

--- src/components/Projects/project.js
+++ src/components/Projects/project.js
@@ -5,8 +5,8 @@ import howto from '../../assets/img/projects/howto.png'
 
 const projArray = [
     {name: 'Sleepsta', desc: 'Working in a team within a multi week project, Sleepsta, in tandem with the app, tracks your sleep patterns to help you get better sleep and improve you habits.', img: sleepsta},
-    {name: 'Coordinator Storytelling', desc: 'App to help coordinators and members of charitable organizations share their stories in a localized hub', img: coordinate},
-    {name: 'Pintereach', desc: 'Led a team in the creation of an article saving and categorizing app.', img: pinter},
-    {name: 'How-To', desc: 'Led a team in the creation of a simple repository for tutorials and how tos on anything. Over the course of a week I guided the team and helped them execute the goals of the project.', img: howto}];
+    {name: 'Coordinator Storytelling', desc: 'App to help coordinators and members of charitable organizations share their stories in a localized hub. ', img: coordinate},
+    {name: 'Pintereach', desc: 'Led a team in the creation of an article saving and categorizing app. Allowed you to store your own personal collections of articles for future use/reference.', img: pinter},
+    {name: 'How-To', desc: 'Led a team in the creation of a simple repository for tutorials and <i>how-tos</i> on anything. Over the course of a week I guided the team and helped them execute the goals of the project.', img: howto}];
 
 export default projArray;
