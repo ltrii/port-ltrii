08d365970d3fbc7d24a2580c23875959b58bd8f4
ltrii <lelandtr@gmail.com>
2020-01-03 01:12:07 -0800

    Some more information on new skills

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,6 +1,9 @@
 import React from "react";
 import "./css/content.css";
 
+import larapng from "../assets/img/skills/laravel.png";
+import vuepng from "../assets/img/skills/vuejs.png";
+
 import BlurbDisplay from "./Skills/BlurbDisplay";
 
 export default function Content() {
@@ -9,6 +12,21 @@ export default function Content() {
       <h3>Skills</h3>
       <div id="skillsLower">
         <BlurbDisplay />
+        <div id="endText">currently learning:</div>
+        <div id="currentlyLearning">
+          <div className="blurb" data-aos="zoom-in">
+              <div id="bimg">
+                <img src={vuepng} alt="Vue" />
+              </div>
+              <div id="btxt">Vue</div>
+            </div>
+            <div className="blurb" data-aos="zoom-in">
+              <div id="bimg">
+                <img src={larapng} alt="Laravel" />
+              </div>
+              <div id="btxt">Laravel</div>
+            </div>
+        </div>
         <div id="endText">...and I'm learning more everyday.</div>
       </div>
     </div>
