798081e24f92c4941b34e79788e2ddfd496980f3
ltrii <lelandtr@gmail.com>
2020-02-08 23:24:07 -0800

    Copy updates

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import "../assets/css/footer.css";
 export default function Footer() {
   return (
     <footer className="footer">
-      <div id="made">made by ltrii (Leland Rogers)</div>
+      <div id="made">made by Leland Rogers</div>
       <div id="thanks">thanks for stopping by</div>
     </footer>
   );
