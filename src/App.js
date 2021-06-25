dc445ad7a4141341b6e9848eefcd2f358ac7ea7f
ltrii <lelandtr@gmail.com>
2021-06-25 01:44:53 -0700

    more cleanup

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 function App() {
   return (
     <div className="portfolio-main">
-      ltrii website under work brb
+      <h1>Leland Thomas Rogers II</h1>
+      <p>I work at <a href="https://www.singlestore.com">SingleStore</a>. This site is under work.</p>
     </div>
   );
 }
