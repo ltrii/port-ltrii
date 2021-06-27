8a58db97854db5321b22ed5a02b112159ea0bdd5
ltrii <lelandtr@gmail.com>
2021-06-26 18:04:53 -0700

    Email improvements

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,10 +5,11 @@ export default function Contact() {
     const [email, setemail] = useState("")
     const [subject, setsubject] = useState("")
     const [message, setmessage] = useState("")
+    const [status, setstatus] = useState("Idle. Waiting to send email...")
 
     const send_email = (e) => {
         e.preventDefault();
-
+        setstatus("Sending email...")
         emailjs.send('service_l1scgwa', 'template_8ydffyq', {
             "email": email,
             "subject": subject,
@@ -16,10 +17,12 @@ export default function Contact() {
         }, 'user_Nt7Az2NoUhoc2zZGlFBNr')
             .then((result) => {
                 console.log(result.text);
+                setstatus("Email sent successfully!")
                 setemail("")
                 setsubject("")
-                setmessage("Sent!")
+                setmessage("")
             }, (error) => {
+                setstatus("Email send failed. My email is lelandtr@gmail.com")
                 console.log(error.text);
             });
     }
@@ -34,7 +37,10 @@ export default function Contact() {
                 <input value={subject} onChange={(e) => setsubject(e.target.value)} />
                 <label>Message</label>
                 <textarea value={message} onChange={(e) => setmessage(e.target.value)} />
-                <button>Send</button>
+                <div id="emailstatus">
+                    <button>Send</button>
+                    <div id="statusdisplay">{status}</div>
+                </div>
             </form>
         </div>
     )
