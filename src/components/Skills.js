0e55874627a9134b3b24f98589a23f4f455a8bb6
ltrii <lelandtr@gmail.com>
2021-12-27 11:32:57 -0800

    Update skills and redo some CSS

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,19 +1,81 @@
 import React from 'react'
 
+import awssvg from './skills/svg/aws.svg'
+import html5svg from './skills/svg/html5.svg'
+import jssvg from './skills/svg/js.svg'
+import k8ssvg from './skills/svg/k8s.svg'
+import mysqlsvg from './skills/svg/mysql.svg'
+import pgsqlsvg from './skills/svg/pgsql.svg'
+import pythonsvg from './skills/svg/python.svg'
+import reactsvg from './skills/svg/react.svg'
+import reduxsvg from './skills/svg/redux.svg'
+import rubysvg from './skills/svg/ruby.svg'
+
 export default function Skills() {
-    const skills_arr = [
-        "AWS", "HTML/CSS","JavaScript", "Python", "C", "Ruby", "React.JS", "Redux", "Express", "MySQL", "Postgres"
-    ]
+
+    const skills_json = {
+        "aws": {
+            name: "Amazon Web Services",
+            icon: awssvg,
+            desc: ""
+        },
+        "html5": {
+            name: "HTML5",
+            icon: html5svg,
+            desc: ""
+        },
+        "js": {
+            name: "JavaScript",
+            icon: jssvg,
+            desc: ""
+        },
+        "k8s": {
+            name: "Kubernetes",
+            icon: k8ssvg,
+            desc: ""
+        },
+        "mysql": {
+            name: "MySQL",
+            icon: mysqlsvg,
+            desc: ""
+        },
+        "pgsql": {
+            name: "PostgresQL",
+            icon: pgsqlsvg,
+            desc: ""
+        },
+        "python": {
+            name: "Python",
+            icon: pythonsvg,
+            desc: ""
+        },
+        "react": {
+            name: "React.JS",
+            icon: reactsvg,
+            desc: ""
+        },
+        "redux": {
+            name: "Redux",
+            icon: reduxsvg,
+            desc: ""
+        },
+        "ruby": {
+            name: "Ruby",
+            icon: rubysvg,
+            desc: ""
+        },
+    }
     return (
         <div id="skills">
-            <ul>
-                {skills_arr.map((ok) => {
+
+                {Object.keys(skills_json).map((key) => {
                     return (
-                        <li>{ok}</li>
+                        <div id="askill">
+                            <img src={skills_json[key]['icon']} alt={skills_json[key]['name']}  />     
+                            {skills_json[key]['name']}             
+                        </div>
                     )
                 })}
-            </ul>
-            ...and more.
         </div>
     )
 }
--- src/components/TopContent.js
+++ src/components/TopContent.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 export default function TopContent() {
     return (
-        <div id="top-content">
-            <p><b>Hello!</b> My name is <i>Leland Rogers</i> and I work at <a href="https://www.singlestore.com" id="s2">SingleStore</a>. I'm currently a support engineer helping our customers troubleshoot highlevel problems within their clusters. I've also got skills in React, and attended Lambda School where I learned the full web stack. Alongside that I have skill with Python, C, and other widely used languages.</p>
+        <div id="topcontent">
+            <div>
+                <p><b>Hello!</b> My name is <i>Leland Rogers</i> and I work at <a href="https://www.singlestore.com" id="s2">SingleStore</a>. I'm currently a support engineer helping our customers troubleshoot highlevel problems within their clusters. I've also got skills in React, and attended Lambda School where I learned the full web stack. Alongside that I have skill with Python, C, and other widely used languages.</p>
+                <p>Most recently I've been utilizing Kubernetes to create and maintain orchestrated deployments of SingleStore and other applications within cloud environments.</p>
+            </div>
             <p id="tooltip">Not your color? Refresh to get a new background.</p>
         </div>
     )
